94ae09edf8d5781824f6eec9a64e4dc68084418f
shyeon <sanghyeonlee137@gmail.com>
2023-01-27 00:23:37 +0900

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Sanghyeon Lee on 2023/01/27.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
