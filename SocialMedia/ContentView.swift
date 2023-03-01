513956c7765cfcc4a03925ce06a63cc747e2fba6
shyeon <sanghyeonlee137@gmail.com>
2023-03-01 16:08:55 +0900

    Add ProfileView

--- SocialMedia/ContentView.swift
+++ SocialMedia/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         // MARK: Redirection User Based on Log Status
         if logStatus {
-            Text("Main View")
+            MainView()
         } else {
             LoginView()
         }
